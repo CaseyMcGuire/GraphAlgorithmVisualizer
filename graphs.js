ff12a0a8b71533c6aa16d6ad3f4cf692475c80e6
Casey McGuire <casey.jay.mcguire@gmail.com>
2015-03-15 15:53:12 -0700

    initialization is setup

--- graphs.js
+++ graphs.js
@@ -1,11 +1,30 @@
 'use strict';//let's catch our errors early
 var SIMGraphs = function(){};
-
+var DEBUG = true;
 
 SIMGraphs.go = function() {
-    var canvases = document.getElementByTagName("canvas");
+    //Adam uses getElementByTagName
+    var canvases = document.getElementsByClassName("graph-canvas");
     for(var i = 0; i < canvases.length; i++){
-	if(canvases[i].getAttribute("data-program") === "graphs") new SIMGraphs.GraphCanvas(canvases[i]);
+//	if(canvases[i].getAttribute("data-program") === "graphs") new SIMGraphs.GraphCanvas(canvases[i]);
+	assert(canvases.length == 1);
     }
 
 }
+
+
+//An assertion function for testing
+//http://stackoverflow.com/questions/15313418/javascript-assert
+function assert(condition, message){
+    if(!DEBUG) return;//I added this
+    if(!condition){
+	message = message || "Assertion failed";
+	if(typeof Error !== "undefined"){
+	    throw new Error(message);
+	}
+	throw message;
+    }
+}
+
+//when the window loads, start your engines.
+window.addEventListener('load', SIMGraphs.go, false);
